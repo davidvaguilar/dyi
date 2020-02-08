b9f51268e7067f7acd5d0f87274f159b4bb184bf
David Villegas Aguilar <david.villegas.aguilar@gmail.com>
2020-02-08 00:21:00 -0400

    Agregada link de Proyectos

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -59,11 +59,14 @@
           <a class="nav-link js-scroll-trigger" href="#experiencia">Experiencia</a>
         </li>
         <li class="nav-item">
+          <a class="nav-link js-scroll-trigger" href="#proyecto">Proyectos</a>
+        </li>
+        <li class="nav-item">
           <a class="nav-link js-scroll-trigger" href="#educacion">Educación</a>
         </li>
         <li class="nav-item">
           <a class="nav-link js-scroll-trigger" href="#intranet">Habilidades</a>
-        </li>
+        </li>        
         <li class="nav-item">
           <a class="nav-link js-scroll-trigger" href="curriculum.php" target="_black">Imprimir Curriculum</a>
         </li>
@@ -179,6 +182,72 @@
             <span class="text-primary">Mayo 2013 - Enero 2015</span>
           </div>
         </div>
+      </div>
+    </section>
+
+    <hr class="m-0">
+    <section class="resume-section p-3 p-lg-5 d-flex justify-content-center" id="proyecto">
+      <div class="w-100">
+        <h2 class="mb-5">Proyectos</h2>
+        <!--<div class="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
+          <div class="resume-content">
+            <h3 class="mb-0">Desarrollador de Sistema</h3>
+            <div class="subheading mb-3">Hospital Iquique - Dr. Ernesto Torres Galdames</div>
+            <p>
+              <ul>
+            		<li>Análisis de sistemas orientado a procesos</li>
+	              <li>Programación de sistemas en lenguaje Php y Javascript.</li>
+	              <li>Dominio de sistema control de versiones Git.</li>
+              </ul>
+            </p>
+          </div>
+          <div class="resume-date text-md-right">
+            <span class="text-primary">Octubre 2017 - Presente</span>
+          </div>
+        </div>-->
+
+        <div class="row">
+          <div class="col-md-4">
+            <div class="card mb-4 box-shadow">
+              <a href="http://tienda.dyi.cl/" title="Sistema de Inventario">
+                <img class="card-img-top" alt="Thumbnail [100%x225]" src="./img/tienda.jpg" data-holder-rendered="true">               
+              </a>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="card mb-4 box-shadow">
+              <a href="http://compra.dyi.cl/" title="Sistema de Ordenes de compra">
+                <img class="card-img-top" alt="Thumbnail [100%x225]" src="./img/carrito.jpg" data-holder-rendered="true" style="height: 225px; width: 100%; display: block;">
+              </a>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="card mb-4 box-shadow">
+              <a href="http://cita.dyi.cl/" title="Sistema de Reporte de Incidencia">
+                <img class="card-img-top" alt="Thumbnail [100%x225]" src="./img/agenda.jpg" data-holder-rendered="true" style="height: 225px; width: 100%; display: block;">
+              </a>
+            </div>
+          </div>
+
+          <div class="col-md-4">
+            <div class="card mb-4 box-shadow">
+              <a href="http://incidencia.dyi.cl/" title="Sistema de Reporte de Incidencia">
+                <img class="card-img-top" alt="Thumbnail [100%x225]" src="./img/incidencia.jpg" data-holder-rendered="true" style="height: 225px; width: 100%; display: block;">
+              </a>
+            </div>
+          </div>
+          <!--<div class="col-md-4">
+            <div class="card mb-4 box-shadow">
+              <img class="card-img-top" data-src="holder.js/100px225?theme=thumb&amp;bg=55595c&amp;fg=eceeef&amp;text=Thumbnail" alt="Thumbnail [100%x225]" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22208%22%20height%3D%22225%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20208%20225%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_17022dc8808%20text%20%7B%20fill%3A%23eceeef%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A11pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_17022dc8808%22%3E%3Crect%20width%3D%22208%22%20height%3D%22225%22%20fill%3D%22%2355595c%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%2266.9453125%22%20y%3D%22117.3%22%3EThumbnail%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" data-holder-rendered="true" style="height: 225px; width: 100%; display: block;">
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="card mb-4 box-shadow">
+              <img class="card-img-top" data-src="holder.js/100px225?theme=thumb&amp;bg=55595c&amp;fg=eceeef&amp;text=Thumbnail" alt="Thumbnail [100%x225]" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22208%22%20height%3D%22225%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20208%20225%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_17022dc880c%20text%20%7B%20fill%3A%23eceeef%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A11pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_17022dc880c%22%3E%3Crect%20width%3D%22208%22%20height%3D%22225%22%20fill%3D%22%2355595c%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%2266.9453125%22%20y%3D%22117.3%22%3EThumbnail%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" data-holder-rendered="true" style="height: 225px; width: 100%; display: block;">
+            </div>
+          </div>-->
+        </div>
+
       </div>
     </section>
 
